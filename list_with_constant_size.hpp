7619d4826feea7dd7b5e555b8385f783b0d75000
Aaron McDaid <aaronmcdaid@gmail.com>
2011-06-03 22:19:35 +0100

    templateized list_with_constant_size, still not tested though

--- list_with_constant_size.hpp
+++ list_with_constant_size.hpp
@@ -6,45 +6,46 @@
 
 namespace misc {
 
-struct list_of_ints : private std :: list<int32_t> {
+template <typename T>
+struct list_with_constant_size : private std :: list<T> {
 	size_t sz;
-	typedef std :: list<int32_t> :: const_iterator const_iterator;
-	typedef std :: list<int32_t> :: iterator iterator;
-	typedef std :: list<int32_t> :: const_reference const_reference;
+	typedef std :: list<T> :: const_iterator const_iterator;
+	typedef std :: list<T> :: iterator iterator;
+	typedef std :: list<T> :: const_reference const_reference;
 
-	explicit list_of_ints() : std :: list<int32_t>(), sz(0) {
+	explicit list_with_constant_size() : std :: list<T>(), sz(0) {
 	}
 
-	const std :: list<int32_t> & get() const {
+	const std :: list<T> & get() const {
 		return *this;
 	}
-	void push_back(int32_t v) {
+	void push_back(T v) {
 		++ sz;
-		this -> std :: list<int32_t> :: push_back(v);
+		this -> std :: list<T> :: push_back(v);
 	}
 	size_t size() const {
-		// assert (sz == this -> std :: list<int32_t> :: size()) ;
+		// DON'T ENABLE THIS ASSERT, IT WOULD DEFEAT THE PURPOSE. Unless you're testing of course // assert (sz == this -> std :: list<T> :: size()) ;
 		return sz;
 	}
 	bool empty() const {
-		return this -> std :: list<int32_t> :: empty();
+		return this -> std :: list<T> :: empty();
 	}
 	iterator begin() {
-		return this -> std :: list<int32_t> :: begin();
+		return this -> std :: list<T> :: begin();
 	}
 	iterator end() {
-		return this -> std :: list<int32_t> :: end();
+		return this -> std :: list<T> :: end();
 	}
 	iterator erase(iterator i) {
 		-- sz;
-		return this -> std :: list<int32_t> :: erase(i);
+		return this -> std :: list<T> :: erase(i);
 	}
-	void insert(iterator i, int32_t v) {
+	void insert(iterator i, T v) {
 		++ sz;
-		this -> std :: list<int32_t> :: insert(i, v);
+		this -> std :: list<T> :: insert(i, v);
 	}
 private:
-	list_of_ints(const std :: list<int32_t> &) {} // this should never happen. It'd be a problem as this->sz would then be wrong, I think.
+	list_with_constant_size(const std :: list<T> &) {} // this should never happen. It'd be a problem as this->sz would then be wrong, I think.
 
 };
 
